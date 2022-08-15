1008eaecb2a35126fd00cff28daf89e1ed2bed5f
Andrew Tomaka <andrew@oak.tech>
2022-08-15 09:58:28 -0400

    Fix inconsistent tabs

--- test/functional/rpc.test.ts
+++ test/functional/rpc.test.ts
@@ -9,18 +9,18 @@ beforeEach(() => {
 
 test('scheduler.getTimeAutomationFees works', async () => {
   const scheduler = new Scheduler(OakChains.STUR);
-	const fee = await scheduler.getTimeAutomationFees(AutomationAction.Notify, 3);
-	expect(fee > 0).toEqual(true);
+  const fee = await scheduler.getTimeAutomationFees(AutomationAction.Notify, 3);
+  expect(fee > 0).toEqual(true);
 });
 
 test('scheduler.calculateOptimalAutostaking works', async () => {
   const scheduler = new Scheduler(OakChains.STUR);
-	const result = await scheduler.calculateOptimalAutostaking(10000000000, "691Fmzb8rhYmBxLvaqYEUApK22s3o6eCzC4whDY7dZZ83YYQ");
-	expect(_.isNil(result)).toEqual(false);
+  const result = await scheduler.calculateOptimalAutostaking(10000000000, "691Fmzb8rhYmBxLvaqYEUApK22s3o6eCzC4whDY7dZZ83YYQ");
+  expect(_.isNil(result)).toEqual(false);
 });
 
 test('scheduler.getAutoCompoundDelegatedStakeTaskIds works', async () => {
   const scheduler = new Scheduler(OakChains.STUR);
-	const result = await scheduler.getAutoCompoundDelegatedStakeTaskIds("68vqVx27xVYeCkqJTQnyXrcMCaKADUa7Rywn9TSrUZyp4NGP");
+  const result = await scheduler.getAutoCompoundDelegatedStakeTaskIds("68vqVx27xVYeCkqJTQnyXrcMCaKADUa7Rywn9TSrUZyp4NGP");
   expect(_.isArray(result)).toEqual(true);
 });
